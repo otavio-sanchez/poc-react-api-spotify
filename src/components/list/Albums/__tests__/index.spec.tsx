05ab8752c5bdec93db9d9474d465f41c7c056f9f
Otávio Sanchez <sanchezotavio@gmail.com>
2020-04-14 08:04:49 -0300

    test/ testes componente list

---
+++ src/components/list/Albums/__tests__/index.spec.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import { Albums } from '../..'
+import albums from '../../../../constants/mocks/albums'
+import { Container } from '../../../grid'
+import Album from '../components/Album'
+import { Text } from '../../../Typography'
+
+describe('Albums', () => {
+  const props = {
+    data: albums,
+    title: 'Metálica',
+    height: 360,
+    onClickItem: () => {},
+    loading: false,
+  }
+
+  it('renders without crashing given the required props', () => {
+    const wrapper = shallow(<Albums {...props} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('should not render component when was loading', () => {
+    const wrapper = shallow(<Albums {...props} />)
+    wrapper.setProps({ loading: true })
+    expect(wrapper.find(Container)).toHaveLength(0)
+  })
+
+  it('should render three Album component', () => {
+    const wrapper = shallow(<Albums {...props} />)
+    expect(wrapper.find(Album)).toHaveLength(3)
+  })
+
+  it('should render feedback when there is not data', () => {
+    const wrapper = shallow(<Albums {...props} />)
+    wrapper.setProps({ data: [] })
+    expect(wrapper.find(Text).get(1)).toContain("Nada encontrado.")
+  })
+})
---
+++ src/constants/mocks/albums.ts
@@ -0,0 +1,85 @@
+const albums = [
+    {
+        images: [
+            {
+                height: 640,
+                url: 'https://i.scdn.co/image/ab67616d0000b273af07dc851962508661bbcfce',
+                width: 640,
+            },
+            {
+                height: 300,
+                url: 'https://i.scdn.co/image/ab67616d00004851af07dc851962508661bbcfce',
+                width: 300,
+            },
+            {
+                height: 64,
+                url: 'https://i.scdn.co/image/ab67616d0000b273af07dc851962508661bbcfce',
+                width: 64,
+            },
+        ],
+        id: '0',
+        name: 'Album foo',
+        artists: [
+            {
+                id: '0',
+                name: 'artist foo',
+            },
+        ],
+    },
+    {
+        images: [
+            {
+                height: 640,
+                url: 'https://i.scdn.co/image/ab67616d0000b273af07dc851962508661bbcfce',
+                width: 640,
+            },
+            {
+                height: 300,
+                url: 'https://i.scdn.co/image/ab67616d00004851af07dc851962508661bbcfce',
+                width: 300,
+            },
+            {
+                height: 64,
+                url: 'https://i.scdn.co/image/ab67616d0000b273af07dc851962508661bbcfce',
+                width: 64,
+            },
+        ],
+        id: '1',
+        name: 'Album bar',
+        artists: [
+            {
+                id: '1',
+                name: 'artist bar',
+            },
+        ],
+    },
+    {
+        images: [
+            {
+                height: 640,
+                url: 'https://i.scdn.co/image/ab67616d0000b273af07dc851962508661bbcfce',
+                width: 640,
+            },
+            {
+                height: 300,
+                url: 'https://i.scdn.co/image/ab67616d00004851af07dc851962508661bbcfce',
+                width: 300,
+            },
+            {
+                height: 64,
+                url: 'https://i.scdn.co/image/ab67616d0000b273af07dc851962508661bbcfce',
+                width: 64,
+            },
+        ],
+        id: '2',
+        name: 'Album baz',
+        artists: [
+            {
+                id: '2',
+                name: 'artist baz',
+            },
+        ],
+    },
+];
+
+export default albums;
